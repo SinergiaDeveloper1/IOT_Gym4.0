7b97a680f9cbb7db0e5b2cb029edbf9f287c638d
Luca Cinti <lucacinti.urbino@gmail.com>
2022-03-01 16:07:47 +0100

    10 righe

--- ACCELEROMETRI/ACC_WIP/ACC_WIP.ino
+++ ACCELEROMETRI/ACC_WIP/ACC_WIP.ino
@@ -1,8 +1,8 @@
 #include <Stepper.h>
 
-#include <WiFi.h> /* inclusione della libreria per il WiFi */
-#include <InfluxDbClient.h>   /* per libreria di comunicazione con InfluxDb */
-#include <InfluxDbCloud.h>    /* per gestire il token di sicurezza di InfluxDb */
+#include <WiFi.h>           /* inclusione della libreria per il WiFi */
+#include <InfluxDbClient.h> /* per libreria di comunicazione con InfluxDb */
+#include <InfluxDbCloud.h>  /* per gestire il token di sicurezza di InfluxDb */
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
@@ -12,21 +12,17 @@
 Adafruit_MPU6050 mpu;
 
 /* costanti per le connessioni */
-#define WIFI_SSID       "Vodafone-A47203440"
-#define WIFI_PASSWORD   "HtmMgyffEM4Mf4cH"
+#define WIFI_SSID "Vodafone-A47203440"
+#define WIFI_PASSWORD "HtmMgyffEM4Mf4cH"
 
-#define INFLUXDB_URL    "http://93.186.254.118:8086"
-#define INFLUXDB_ORG    "uniurb"
-#define INFLUXDB_BUCKET "esercitazioni" 
-#define INFLUXDB_TOKEN  "0s5ZuEkoUpmjulLxgpZfnXeiN1RU5tafsDkZ_bSdB-DzeDXQeN8k03ylXMREBwqYKd46oLq0Se8Qc13IjOuF-A=="
+#define INFLUXDB_URL "http://93.186.254.118:8086"
+#define INFLUXDB_ORG "uniurb"
+#define INFLUXDB_BUCKET "esercitazioni"
+#define INFLUXDB_TOKEN "0s5ZuEkoUpmjulLxgpZfnXeiN1RU5tafsDkZ_bSdB-DzeDXQeN8k03ylXMREBwqYKd46oLq0Se8Qc13IjOuF-A=="
 
-#define D_TARA    100
-#define D_MISURE  40
+#define D_MISURE 10
 
-/* tare accelerometro SX */
-float taraX,
-      taraY,
-      taraZ;
+/* in questa versione ho rimosso la tara sugli accelerometri */
 
 /* variabili globali */
 InfluxDBClient client(INFLUXDB_URL,
@@ -38,7 +34,8 @@ InfluxDBClient client(INFLUXDB_URL,
 /* definisco la tabella influxDb dove inserire i dati */
 Point sensor("progetto_LC");
 
-void setup() {
+void setup()
+{
 
   Serial.begin(115200);
 
@@ -47,7 +44,8 @@ void setup() {
 
   Serial.print("Connecting to WiFi ..");
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print('.');
     delay(1000);
   }
@@ -61,14 +59,18 @@ void setup() {
   sensor.addTag("room", "Palestra");
 
   /* testo la connessione */
-  if (client.validateConnection()) {
+  if (client.validateConnection())
+  {
     Serial.println("Connected to influxDb");
-  } else {
+  }
+  else
+  {
     Serial.println("Connection failed");
     Serial.println(client.getLastErrorMessage());
   }
 
-  if (!mpu.begin()) {
+  if (!mpu.begin())
+  {
     Serial.println("Failed to find MPU6050 chip");
     while (1);
   }
@@ -80,14 +82,12 @@ void setup() {
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
   Serial.println("");
-  delay(1000);
 
+  /* prima di passare al loop, aspetto 5 secondi */
+  delay(5000);
 }
 
 /* variabili globali di elaborazione dati */
-float perTaraX[D_TARA];
-float perTaraY[D_TARA];
-float perTaraZ[D_TARA];
 float AccX[D_MISURE];
 float AccY[D_MISURE];
 float AccZ[D_MISURE];
@@ -95,168 +95,113 @@ float AccZ[D_MISURE];
 float aX, aY, aZ; /* valori accelerazione */
 
 /* contatori */
-int cTara = 0,
-    cRaccoltaDati = 0;
-
-bool flgTaraInCorso = true;
-bool flgInizioDiscesa = false;
-bool flgInizioSalita = false;
+int cRaccoltaDati = 0;
 
 /* INIZIO LOOP */
-void loop() {
+void loop()
+{
 
   readSensors();
 
-  /* taro i sensori in base alla loro posizione */
-  if (flgTaraInCorso) {
+  /* raccolgo i dati */
 
-    impostaTara();
+  if (cRaccoltaDati < D_MISURE)
+  {
 
-  } else {
+    /* raccolgo i dati della discesa */
+    AccX[cRaccoltaDati] = aX;
+    AccY[cRaccoltaDati] = aY;
+    AccZ[cRaccoltaDati] = aZ;
 
-    if (cRaccoltaDati < D_MISURE) {
-
-      /* raccolgo i dati della discesa */
-      AccX[cRaccoltaDati] = aX;
-      AccY[cRaccoltaDati] = aY;
-      AccZ[cRaccoltaDati] = aZ;
-
-      cRaccoltaDati++;
-
-    } else {
-
-      /* stampo sulla seriale i valori raccolti */
-      Serial.print("Accelerazione X: ");
-      
-      for (byte i = 0; i < D_MISURE; i++) {    
-        Serial.print(AccX[i]);
-        Serial.print(" ");
-      }
-
-      Serial.println("");
-
-      Serial.print("Accelerazione Y: ");
-      
-      for (byte i = 0; i < D_MISURE; i++) {   
-        Serial.print(AccY[i]);
-        Serial.print(" ");
-      }
-
-      Serial.println("");
-
-      Serial.print("Accelerazione Z: ");
-      for (byte i = 0; i < D_MISURE; i++) {      
-        Serial.print(AccZ[i]);
-        Serial.print(" ");
-      }
-
-      Serial.println("");
-
-      /* elaboro i dati e li invio a InfluxDB */
-      cRaccoltaDati = 0;
-      writeToInfluxDb(elaboraDato());
-
-    }
-
-    delay(10);
-
+    cRaccoltaDati++;
   }
+  else
+  {
 
-}
+    /* stampo sulla seriale i valori raccolti */
+    Serial.print("Accelerazione X: ");
 
-void impostaTara() {
-
-  if (cTara < D_TARA) {
-
-    perTaraX[cTara] = aX;
-    perTaraY[cTara] = aY;
-    perTaraZ[cTara] = aZ;
-
-    cTara++;
-    Serial.println(cTara);
-
-  } else {
-
-    /* calcolo la tara come valore medio di quelli registrati */
-    for (byte i = 0; i < D_TARA; i++) {
-      taraX += perTaraX[i];
-      taraY += perTaraY[i];
-      taraZ += perTaraZ[i];
+    for (byte i = 0; i < D_MISURE; i++)
+    {
+      Serial.print(AccX[i]);
+      Serial.print(" ");
     }
 
-    taraX = taraX / D_TARA;
-    taraY = taraY / D_TARA;
-    taraZ = taraZ / D_TARA;
-
-    Serial.println("Tara nell'asse X: " + String(taraX, 2));
-    Serial.println("Tara nell'asse Y: " + String(taraY, 2));
-    Serial.println("Tara nell'asse Z: " + String(taraZ, 2));
     Serial.println("");
 
-    flgTaraInCorso = false;
+    Serial.print("Accelerazione Y: ");
 
+    for (byte i = 0; i < D_MISURE; i++)
+    {
+      Serial.print(AccY[i]);
+      Serial.print(" ");
+    }
+
+    Serial.println("");
+
+    Serial.print("Accelerazione Z: ");
+    for (byte i = 0; i < D_MISURE; i++)
+    {
+      Serial.print(AccZ[i]);
+      Serial.print(" ");
+    }
+
+    Serial.println("");
+
+    /* elaboro i dati e li invio a InfluxDB */
+    cRaccoltaDati = 0;
+    writeToInfluxDb(elaboraDato());
+
+    delay(10);
   }
-
 }
 
-float elaboraDato() {
+float elaboraDato()
+{
 
   float accelerazioneMediaTot = 0.0;
   float accX = 0.0;
   float accY = 0.0;
   float accZ = 0.0;
 
-  /* calcolo la tara come valore medio di quelli registrati */
-  for (byte i = 0; i < D_MISURE; i++) {
+  for (byte i = 0; i < D_MISURE; i++)
+  {
     accX += AccX[i];
     accY += AccY[i];
     accZ += AccZ[i];
   }
 
-  accX /= D_MISURE;  
+  accX /= D_MISURE;
   accY /= D_MISURE;
   accZ /= D_MISURE;
   Serial.println("accX: " + String(accX, 4));
   Serial.println("accY: " + String(accY, 4));
   Serial.println("accZ: " + String(accZ, 4));
 
-  accelerazioneMediaTot = sqrt((accX * accX) + (accY * accY) + (accZ * accZ));
+  accelerazioneMediaTot = sqrt((accX * accX) + (accY * accY) + (accZ * accZ)) - 9.81;
   accelerazioneMediaTot = roundf(accelerazioneMediaTot * 10000) / 10000;
-  
+
   Serial.println("Accelerazione bilanciere Sx: " + String(accelerazioneMediaTot, 4));
 
   return (accelerazioneMediaTot);
-
 }
 
 /* funzione che legge dal sensore */
-void readSensors() {
+void readSensors()
+{
 
   /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  aX = a.acceleration.x - taraX;
-  aY = a.acceleration.y - taraY;
-  aZ = a.acceleration.z - taraZ;
-
-  /* Print out the values */
-  /*
-  Serial.print("Acceleration X: ");
-  Serial.print(aX);
-
-  Serial.print(", Y: ");
-  Serial.print(aY);
-
-  Serial.print(", Z: ");
-  Serial.print(aZ);
-
-  Serial.println(" m/s^2"); 
-  */
+  aX = a.acceleration.x;
+  aY = a.acceleration.y;
+  aZ = a.acceleration.z;
 
 }
 
-void writeToInfluxDb(float a) {
+void writeToInfluxDb(float a)
+{
 
   sensor.clearFields();
   sensor.addField("Accelerazione Sx", a);
@@ -264,10 +209,9 @@ void writeToInfluxDb(float a) {
   Serial.print("Writing: ");
   Serial.println(sensor.toLineProtocol());
 
-  if (!client.writePoint(sensor)) {
+  if (!client.writePoint(sensor))
+  {
     Serial.print("InfluxDB write failed ");
     Serial.println(client.getLastErrorMessage());
   }
-
-
 }
